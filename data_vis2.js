1436267f36db6df109d092f5c851ee940a3b3732
raedan9 <raedan.fernandez@gmail.com>
2025-10-24 07:22:15 +1100

    Finished Visualisation

--- data_vis2.js
+++ data_vis2.js
@@ -1,9 +1,24 @@
-var vg_1 = "gdp_bar_chart.vg.json";
+var vg_1 = "pop_sex_bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 
 }).catch(console.error);
 
 var vg_2 = "gsp_map.vg.json";
 vegaEmbed("#map_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "pop_time.vg.json";
+vegaEmbed("#line_chart", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "pop_density.vg.json";
+vegaEmbed("#scatter_chart", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "pop_age.vg.json";
+vegaEmbed("#pyramid_chart", vg_5).then(function(result) {
 
 }).catch(console.error);
