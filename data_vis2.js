3bf35c603e9eef16cf15522344e21dea42f29e97
raedan9 <raedan.fernandez@gmail.com>
2025-10-24 02:33:51 +1100

    Made more charts

--- data_vis2.js
+++ data_vis2.js
@@ -2,3 +2,8 @@ var vg_1 = "gdp_bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+var vg_2 = "gsp_map.vg.json";
+vegaEmbed("#map_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
